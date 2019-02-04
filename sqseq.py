a7defb7311d5fe394d665a301a9d53ce9bb9d4e2
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-02-04 15:48:36 +0530

    Create sqseq.py

---
+++ sqseq.py
@@ -0,0 +1,9 @@
+#kljskfksdf
+a1,a2=map(int,input().split( ))
+b1,b2=map(int,input().split( ))
+c1,c2=map(int,input().split( ))
+d1,d2=map(int,input().split( ))
+if a1==a2==b1==d2 and b2==c1==c2==d1:
+    print("yes")
+else:
+    print("no")
