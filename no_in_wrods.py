842ed5a9933a43b9ed68dbfee1e2b3ab568cd023
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-05-15 11:15:48 +0530

    Create no_in_wrods.py

---
+++ no_in_wrods.py
@@ -0,0 +1,20 @@
+#kjgjiod
+n=int(input())
+if(n==1):
+	print("One")
+elif(n==2):
+	print("Two")
+elif(n==3):
+	print("Three")
+elif(n==4):
+	print("Four")
+elif(n==5):
+	print("Five")
+elif(n==6):
+	print("Six")
+elif(n==7):
+	print("Seven")
+elif(n==8):
+	print("Eight")
+elif(n==9):
+	print("Nine")
