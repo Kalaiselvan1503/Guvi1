9ed89ff6834b94f50b6215f0a89eaa0ca62376ee
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-05-15 09:44:06 +0530

    Create prime_not.py

---
+++ prime_not.py
@@ -0,0 +1,12 @@
+#jiohhdfujigo
+num =int(input())
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("no")
+           break
+   else:
+       print("yes")
+       
+else:
+   print("no")
