f611c5fc49d73c31b8cb9bf0a3f40a0db8b8e957
Kalaiselvan_1503 <30726230+Kalaiselvan1503@users.noreply.github.com>
2019-07-09 19:00:17 +0530

    Create long_sub_array.py

---
+++ long_sub_array.py
@@ -0,0 +1,12 @@
+#hihdigj\
+m=int(input())
+n=list(map(int,input().split()))
+o=[]
+p=1
+for i in n:
+  if i not in o:
+    o.append(i)
+for i in range(len(o)-1):
+  if (o[i]<o[i+1]):
+    p+=1
+print(p)
